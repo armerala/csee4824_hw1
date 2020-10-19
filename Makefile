2b0aff07cf20374c32035416016b6556703f60c6
armerala <aa3938@columbia.edu>
2020-10-19 13:57:01 +0100

    Adds makefile and initial code tweaked to compile

---
+++ Makefile
@@ -0,0 +1,21 @@
+# Compile settings
+CC      := gcc
+CCFLAGS := -Wall --std=gnu17
+LDFLAGS :=
+TARGET  := memtest
+
+# Make targets
+$(TARGET): main.o
+	$(CC) $(LDFLAGS) -o $(TARGET) main.o 
+
+main.o: main.c
+	$(CC) -c $(CCFLAGS) main.c
+
+
+# Phony targets
+.PHONY: all clean
+
+all: $(TARGET)
+
+clean: 
+	rm -rf $(TARGET) ./*.o
---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define REPEAT 1000000
+
+// flush cache line
+static inline void clflush(volatile void *p)
+{
+    asm volatile ("clflush (%0)" :: "r"(p));
+}
+
+// fetch read-time stamp counter
+static inline uint64_t rdtsc()
+{
+    unsigned long a, d;
+    asm volatile ("rdtsc" : "=a" (a), "=d" (d));
+    return a | ((uint64_t)d << 32);
+}
+
+char lineBuffer[64];
+long int rep;
+
+static inline void memtest()
+{
+    uint64_t start, end, clock;
+    char* lineBuffer = (char*) malloc(64);
+    char* lineBufferCopy = (char*) malloc(64);
+
+    // populate line buf
+    for (int i = 0; i < 64; i++) {
+        lineBuffer[i] = '1';
+    }
+    clock = 0;
+
+    // clock memcpy for line buf REPEAT times
+    for (rep = 0; rep < REPEAT; rep++) {
+
+        start = rdtsc();
+        memcpy(lineBufferCopy, lineBuffer, 64);
+        end = rdtsc();
+
+        clflush(lineBuffer);
+        clflush(lineBufferCopy);
+        clock = clock + (end - start);
+
+        printf("%llu ticks to copy 64B\n", (end-start));
+    }
+
+    printf("took %llu ticks total\n", clock);
+}
+
+// main
+int main(int ac, char **av)
+{
+    printf("------------------------------\n");
+    memtest();
+    return 0;
+}
